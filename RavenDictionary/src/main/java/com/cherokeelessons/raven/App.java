e4dd2d571406bff1e7694230e99e7dcf61dffd4b
mjoyner <mjoyner@vbservices.net>
2015-12-16 20:15:35 -0500

    output csv file for use by cherokeedictionary.net

--- RavenDictionary/src/main/java/com/cherokeelessons/raven/App.java
+++ RavenDictionary/src/main/java/com/cherokeelessons/raven/App.java
@@ -3,11 +3,13 @@ package com.cherokeelessons.raven;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringEscapeUtils;
+import org.apache.commons.lang3.StringUtils;
 
 public class App extends Thread {
 
@@ -29,16 +31,14 @@ public class App extends Thread {
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
-		App.info("creating new csv file...");
-		entries = parseDictionary.getEntries();
-		Iterator<IEntry> iterator = entries.iterator();
+		App.info("creating new csv file for use by analyzer project ...");
 		List<String> csvlist = new ArrayList<>();
-		while (iterator.hasNext()) {
-			IEntry next = iterator.next();
-			String def = next.getDef();
+		entries = parseDictionary.getEntries();
+		entries.forEach(entry->{
+			String def = entry.getDef();
 			def = def.replace("He is ", "");
 			def = def.replace("She is ", "");
-			List<String> syll = next.getSyllabary();
+			List<String> syll = entry.getSyllabary();
 			String main = syll.get(0);
 			for (String s : syll) {
 				if (!s.matches(".*[Ꭰ-Ᏼ].*")) {
@@ -47,16 +47,161 @@ public class App extends Thread {
 				csvlist.add(StringEscapeUtils.escapeCsv(s) + ","
 						+ StringEscapeUtils.escapeCsv(def + " (" + main + ") [raven]"));
 			}
-		}
+		});
 
 		try {
 			FileUtils.writeLines(new File(DIR + "dictionary.csv"), csvlist);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
+		
+		csvlist.clear();
+		App.info("creating new csv file for use by cherokeedictionary.net ...");
+		List<String> columns = new ArrayList<>();
+		columns.add("entry");
+		columns.add("pronounce");
+		columns.add("pos");
+		columns.add("c");
+		columns.add("definitions");
+		
+		columns.add("pres_syl_1st");
+		columns.add("pres_pro_1st");
+		
+		columns.add("past_syl");
+		columns.add("past_pro");
+		
+		columns.add("hab_syl");
+		columns.add("hab_pro");
+		
+		columns.add("imm_syl");
+		columns.add("imm_pro");
+		
+		columns.add("dvb_syl");
+		columns.add("dvb_pro");
+		
+		int verb = columns.size();
+		
+		columns.add("syl_3rd_plural");
+		columns.add("pro_3rd_plural");
+		columns.add("syl_1st");
+		columns.add("pro_1st");
+		columns.add("syl_1st_plural");
+		columns.add("pro_1st_plural");
+		
+		int example = columns.size();
+		
+		columns.add("example_syl");
+		columns.add("example_eng");
+		columns.add("example_note");
+		
+		columns.add("notes");
+		
+		csvlist.add(StringUtils.join(columns, ","));
+		
+		entries.forEach(entry->{
+			columns.clear();
+			
+			int c = entry.getSyllabary().size();
+			Iterator<String> isyl = entry.getSyllabary().iterator();
+			Iterator<String> ipro = entry.getPronunciations().iterator();
+			String def = entry.formattedDefinition(); 
+			String part = entry.getType();
+			Iterator<String> inotes = entry.getNotes().iterator();
+			String genus = entry.getGenus();
+			
+			columns.add(StringEscapeUtils.escapeCsv(isyl.next()));
+			columns.add(StringEscapeUtils.escapeCsv(ipro.next()));
+			columns.add(StringEscapeUtils.escapeCsv(part));
+			columns.add(StringEscapeUtils.escapeCsv(c+""));
+			columns.add(StringEscapeUtils.escapeCsv(def));
+			
+			if (c!=6) {
+				while (columns.size()<verb) {
+					columns.add("");
+				}
+			}
+			
+			if (c==2) {
+				String s = isyl.next();
+				String p = ipro.next();
+				if (!s.matches("[ᏓᏕᏗᏙᏚᏛᏤᏦᏧᏨ].*")){
+					columns.add("");
+					columns.add("");
+				}
+				columns.add(StringEscapeUtils.escapeCsv(s));
+				columns.add(StringEscapeUtils.escapeCsv(p));
+			}
+			
+			while (isyl.hasNext()) {
+				columns.add(StringEscapeUtils.escapeCsv(isyl.next()));
+				columns.add(StringEscapeUtils.escapeCsv(ipro.next()));
+			}
+			
+			while (columns.size()<example) {
+				columns.add("");
+			}
+			
+			if (inotes.hasNext()) {
+				String note = inotes.next();
+				//\series bold
+				//\series default
+				note=note.replace("\n\\series bold\n", "<strong>");
+				note=note.replace("\n\\series default\n", "</strong>");
+				note=note.replace("\n\\emph on\n", "<emph>");
+				note=note.replace("\n\\emph default\n", "</emph>");
+				note=note.replace("\n\\bar under\n", "<u>");
+				note=note.replace("\n\\bar default\n", "</u>");
+				note=note.replace("\n\\begin_inset Newline newline\n\\end_inset\n", "\n");
+				note=note.replaceAll("\n+", "\n");
+				Iterator<String> inote = Arrays.asList(StringUtils.split(note, "\n")).iterator();
+				columns.add(StringEscapeUtils.escapeCsv(inote.next()));
+				if (inote.hasNext()) {
+					columns.add(StringEscapeUtils.escapeCsv(inote.next()));
+				} else {
+					columns.add("");
+				}
+				if (inote.hasNext()) {
+					columns.add(StringEscapeUtils.escapeCsv(StringUtils.join(inote, "<br/>")));
+				} else {
+					columns.add("");
+				}
+			} else {
+				columns.add("");
+				columns.add("");
+				columns.add("");
+			}
+			if (inotes.hasNext()) {
+				String note = StringUtils.join(inotes,"<br/>");
+				note=note.replace("\n\\series bold\n", "<strong>");
+				note=note.replace("\n\\series default\n", "</strong>");
+				note=note.replace("\n\\emph on\n", "<emph>");
+				note=note.replace("\n\\emph default\n", "</emph>");
+				note=note.replace("\n\\bar under\n", "<u>");
+				note=note.replace("\n\\bar default\n", "</u>");
+				note=note.replace("\n\\begin_inset Newline newline\n\\end_inset\n", "<br/>");
+				note=note.replace("\n", "");
+				columns.add(StringEscapeUtils.escapeCsv(note));
+			} else {
+				columns.add("");
+			}
+			csvlist.add(StringUtils.join(columns, ","));
+		});
+		
+		try {
+			FileUtils.writeLines(new File(DIR + "raven-cherokeedictionary-net.csv"), csvlist);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 		App.info("done.");
 	}
+	
+	/*
+	 * definitiond
+	 * entrya
+	 * 
+	 * source
+	 */
 
 	public App(String[] args) {
 	}
