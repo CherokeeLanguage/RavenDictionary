4147b1f293246a4ce06ce9897989d0d47e9a7580
mjoyner <mjoyner@vbservices.net>
2016-07-31 16:27:31 -0400

    depend on simpletextui project

--- RavenDictionary/src/main/java/com/cherokeelessons/raven/Main.java
+++ RavenDictionary/src/main/java/com/cherokeelessons/raven/Main.java
@@ -1,28 +1,19 @@
 package com.cherokeelessons.raven;
 
 import java.awt.EventQueue;
-import java.io.File;
+import java.io.IOException;
 
 import com.cherokeelessons.gui.MainWindow;
 import com.cherokeelessons.gui.MainWindow.Config;
 
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		Config config = new Config() {
-			@Override
-			public File getReportPathFile() {
-				File reportFolder = new File("reports");
-				if (!reportFolder.isDirectory()) {
-					reportFolder.mkdirs();
-				}
-				return reportFolder;
-			}
-			
 			@Override
 			public String getApptitle() {
 				return "Raven Dictionary Rebuilder";
 			}
-			
+
 			@Override
 			public Thread getApp(String... args) throws Exception {
 				return new App(args);
